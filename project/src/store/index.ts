7090177f55488ef109797c6a382aa00db708b6b9
Rimich666 <89179699365@mail.ru>
2023-04-16 20:55:50 +0400

    Добро пожаловать, или посторонним вход воспрещён (часть 2)

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -2,7 +2,7 @@ import {configureStore} from '@reduxjs/toolkit';
 import {createAPI} from '../servises/api';
 import {redirect} from './middlewares/redirect';
 import {RootReducer} from './root-reducer';
-import {logTime} from './middlewares/logTime';
+// import {logTime} from './middlewares/logTime';
 import {setTimerCommentForm} from './middlewares/set-timer-comment-form';
 import {fillFavoriteData} from './middlewares/fill-favorite-data/fill-favorite-data';
 import {changeFavorite} from './middlewares/fill-favorite-data/change-favorite';
