0fb07286781dd630a86091dd39c73597e6085230
Rimich666 <89179699365@mail.ru>
2023-04-21 06:44:59 +0400

    А теперь исправлю то что поломал

---
+++ project/src/components/login-form/login-form-input/login-form-input.ts
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+
+export default function LoginFormInput({onFormInput, type}: InputProps): JSX.Element {
+  const [input, setInput] = useState('');
+  const onInput = (evt: React.FormEvent<HTMLInputElement>) => {
+    setInput(evt.currentTarget.value);
+    onFormInput(evt.currentTarget.value);
+  };
+  return (
+    <input className="login__input form__input" type={type} name={type}
+      placeholder={type[0].toUpperCase().concat(type.substring(1))}
+      value={input} required onInput={onInput}
+    />
+  );
+}
