fbbdddc229118e7ba0840cbf3eff2fa136537965
Rimich666 <89179699365@mail.ru>
2023-04-21 07:00:04 +0400

    А теперь исправлю то что поломал 3

--- project/src/components/room/reviews-form/review-form-text.tsx
+++ project/src/components/room/reviews-form/review-form-text.tsx
@@ -1,14 +1,23 @@
 import {ReviewLength} from '../../../settings';
-import React from 'react';
+import React, {useState} from 'react';
 import {useAppDispatch, useAppSelector} from '../../../hooks';
 import {setReview} from '../../../store/offer/offer';
-import {selectReviewsText} from '../../../store/offer/offer-selectors';
+import {selectBlockReviewForm, selectReviewsText} from '../../../store/offer/offer-selectors';
 
 export default function ReviewFormText(): JSX.Element {
-  const dispatch = useAppDispatch();
-  const {isBlock, text} = useAppSelector(selectReviewsText);
-  const onInputText = (evt: React.FormEvent<HTMLTextAreaElement>) => {
-    dispatch(setReview(evt.currentTarget.value));
+  //const dispatch = useAppDispatch();
+  //const {isBlock, text} = useAppSelector(selectReviewsText);
+  const isBlock = useAppSelector(selectBlockReviewForm);
+  const [text, setText] = useState('');
+  const onInput = (evt: React.FormEvent<HTMLTextAreaElement>) => {
+    const comment = evt.currentTarget.value;
+    setText(comment);
+    onInputText(comment);
+    //dispatch(setReview(evt.currentTarget.value));
+  };
+
+  //const onInputText = (evt: React.FormEvent<HTMLTextAreaElement>) => {
+  //  dispatch(setReview(evt.currentTarget.value));
   };
   return (
     <textarea className="reviews__textarea form__textarea" id="review" name="review"
